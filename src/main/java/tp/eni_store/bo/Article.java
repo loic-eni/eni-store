6a833def5a6d21e443f2a10e06c36181dea0b4f5
Loic DUVAIL <loic.duvail2022@campus-eni.fr>
2025-08-19 11:05:49 +0200

    TP2

---
+++ src/main/java/tp/eni_store/bo/Article.java
@@ -0,0 +1,15 @@
+package tp.eni_store.bo;
+
+public class Article {
+    public int id;
+    public String title;
+
+    public Article(){
+        super();
+    }
+
+    public Article(int id, String title){
+        this.id = id;
+        this.title = title;
+    }
+}
---
+++ src/main/java/tp/eni_store/service/ArticleService.java
@@ -0,0 +1,55 @@
+package tp.eni_store.service;
+
+import org.springframework.cache.support.NullValue;
+import org.springframework.stereotype.Service;
+import tp.eni_store.bo.Article;
+import tp.eni_store.dao.jpa.mock.ArticleDAO;
+
+import java.util.List;
+
+@Service
+public class ArticleService {
+    private final ArticleDAO articleDAO;
+
+    public ArticleService(ArticleDAO articleDAO) {
+        this.articleDAO = articleDAO;
+    }
+
+    public ServiceResponse<List<Article>> getAll(){
+        return new ServiceResponse<>(ServiceCodes.SUCCESS, articleDAO.getArticles());
+    }
+
+    public ServiceResponse<Article> getById(int id){
+        Article article = articleDAO.getArticles().stream().filter(art -> art.id == id).findFirst().orElse(null);
+
+        if(article == null){
+            return new ServiceResponse<>(ServiceCodes.NOT_FOUND, null);
+        }
+        return new ServiceResponse<>(ServiceCodes.SUCCESS, article);
+    }
+
+    public ServiceResponse<NullValue> delete(int id){
+        Article article = articleDAO.getArticle(id);
+
+        if(article == null) {
+            return new ServiceResponse<>(ServiceCodes.NOT_FOUND, null);
+        }
+
+        List<Article> articles = articleDAO.getArticles();
+        articles.removeIf(articl -> articl.id == id);
+        return new ServiceResponse<>(ServiceCodes.SUCCESS, null);
+    }
+
+    public ServiceResponse<Article> save(Article article){
+        List<Article> articles = articleDAO.getArticles();
+        Article match = articles.get(article.id);
+
+        if(match != null) {
+            articles.set(match.id, article);
+            return new ServiceResponse<>(ServiceCodes.UPDATED, article);
+        }
+
+        articles.add(article);
+        return new ServiceResponse<>(ServiceCodes.SUCCESS, article);
+    }
+}
